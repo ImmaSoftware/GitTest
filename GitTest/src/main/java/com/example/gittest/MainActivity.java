5dc929206e451b707276cdada1acc7babcc07f0b
ImmaSoftware <immasoftware@mail.ru>
2014-10-14 16:56:31 +0400

    New comment

--- GitTest/src/main/java/com/example/gittest/MainActivity.java
+++ GitTest/src/main/java/com/example/gittest/MainActivity.java
@@ -1,28 +1,16 @@
 package com.example.gittest;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.*;
 
 public class MainActivity extends Activity {
 
-    @Override
+ @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //New chnge in my project
-		Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_SHORT).show();
-		
     }
-
 
 }
